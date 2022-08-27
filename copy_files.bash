39654ea4ad670692bb58d57d6c73331e0cc395a7
galan-e <55856479+galan-e@users.noreply.github.com>
2022-08-27 09:49:27 -0500

    Add files via upload

---
+++ copy_files.bash
@@ -0,0 +1,35 @@
+#! usr/bin/bash
+echo "Creando Directorios"
+
+mkdir -p prueba_{1..25}
+
+echo "Copiando Archivos a Directorios"
+cp SLI_final.py prueba_1
+cp SLI_final.py prueba_2
+cp SLI_final.py prueba_3
+cp SLI_final.py prueba_4
+cp SLI_final.py prueba_5
+cp SLI_final.py prueba_6
+cp SLI_final.py prueba_7
+cp SLI_final.py prueba_8
+cp SLI_final.py prueba_9
+cp SLI_final.py prueba_10
+cp SLI_final.py prueba_11
+cp SLI_final.py prueba_12
+cp SLI_final.py prueba_13
+cp SLI_final.py prueba_14
+cp SLI_final.py prueba_15
+cp SLI_final.py prueba_16
+cp SLI_final.py prueba_17
+cp SLI_final.py prueba_18
+cp SLI_final.py prueba_19
+cp SLI_final.py prueba_20
+cp SLI_final.py prueba_21
+cp SLI_final.py prueba_22
+cp SLI_final.py prueba_23
+cp SLI_final.py prueba_24
+cp SLI_final.py prueba_25
+
+echo " Copiando archivos a directorios- Done"
+
+
